d3c66dd927e78aa223921970af45d7ad062a912f
ZValeri11 <valerkaluchnikov@yandex.ru>
2023-11-22 14:44:35 +0500

    4ZadaniePassed

---
+++ Test.py
@@ -0,0 +1,30 @@
+import library
+import pytest
+
+# Тесты для функции определения n чисел Фибоначчи
+def test_fibonacci_numbers():
+    assert library.fibonacci_numbers(5) == [0, 1, 1, 2, 3]
+    assert library.fibonacci_numbers(8) == [0, 1, 1, 2, 3, 5, 8, 13]
+
+# Тесты для функции сортировки пузырьком
+def test_bubble_sort():
+    assert library.bubble_sort([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]) == [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]
+    assert library.bubble_sort([9, 8, 7, 6, 5, 4, 3, 2, 1]) == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+# Тесты для функции калькулятора
+def test_calculator():
+    # Корректные входные данные
+    assert library.calculator(5, 3, '+') == 8
+    assert library.calculator(5, 3, '-') == 2
+    assert library.calculator(5, 3, '*') == 15
+    assert library.calculator(6, 2, '/') == 3.0
+    assert library.calculator(6, 0, '/') == "Error: Division by zero"
+    assert library.calculator(6, 2, '@') == "Error: Invalid operation"
+
+    # Граничные значения
+    assert library.calculator(0, 0, '+') == 0
+    assert library.calculator(-1, 1, '-') == -2
+    assert library.calculator(10, 0, '*') == 0
+    assert library.calculator(10, 0, '/') == "Error: Division by zero"
+
+pytest.main()
